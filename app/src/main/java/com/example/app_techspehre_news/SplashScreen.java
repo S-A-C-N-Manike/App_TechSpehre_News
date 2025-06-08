f74e84128b1f2d1e81322f7c1cb155484f5b6f88
S-A-C-N-Manike <167967965+S-A-C-N-Manike@users.noreply.github.com>
2025-06-08 11:38:19 +0530

    finished splash screen
    Add Splash Screen implementation

---
+++ app/src/main/java/com/example/app_techspehre_news/SplashScreen.java
@@ -0,0 +1,23 @@
+package com.example.app_techspehre_news;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.active_splash);
+
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000); // 3 seconds delay
+    }
+}
